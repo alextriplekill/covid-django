137b9a4e8640d7efb6e91989a2811b967649e7be
sanch <sanches1998s@mail.ru>
2020-05-30 12:45:54 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import webbrowser
+import matplotlib.pyplot as plt
+import os
+
+BASE_DIR = dir_path = os.path.dirname(os.path.realpath(__file__))
+BASE_NAME = "table.html"
+path_infected = os.path.join(BASE_DIR, 'graphData\\time_series_covid19_confirmed_global.csv')
+path_recovered = os.path.join(BASE_DIR, 'graphData\\time_series_covid19_recovered_global.csv')
+path_deaths = os.path.join(BASE_DIR, 'graphData\\time_series_covid19_deaths_global.csv')
+path_html = os.path.join(BASE_DIR, 'table.html')
+
+# Create your views here.
+def main():
+    arr_confirm = pd.read_csv(path_infected, sep=',', header=0)
+    arr_confirm.drop(columns=['Lat','Long','Province/State'], inplace=True)
+    arr_confirm["Infected"] = arr_confirm.iloc[:,-1]
+    arr_confirm.drop(list(arr_confirm.filter(regex=r'(\d+/\d+/\d+)')), axis=1, inplace=True)
+    arr_recover = pd.read_csv(path_recovered, sep=',', header=0)
+    arr_recover.drop(columns=['Lat', 'Long', 'Province/State'], inplace=True)
+    arr_confirm["Recovered"] = arr_recover.iloc[:, -1]
+    arr_deaths = pd.read_csv(path_deaths, sep=',', header=0)
+    arr_deaths.drop(columns=['Lat', 'Long', 'Province/State'], inplace=True)
+    arr_confirm["Deaths"] = arr_deaths.iloc[:, -1]
+    file = open(BASE_NAME, "wb")
+
+    data = arr_confirm.to_html().encode('utf-8')
+    file.write(data)
+    file.close()
+    webbrowser.open(path_html)
+    #plot = arr_confirm.plot(kind="barh")
+    #plot.show()
