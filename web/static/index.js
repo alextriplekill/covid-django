ae358883e5b15a158ce8acb95a4aae7b779bdbd3
papacarlos <>
2020-06-03 02:07:20 +0300

    added translating

--- web/static/index.js
+++ web/static/index.js
@@ -1,102 +1,16 @@
-$.i18n({
-  locale: 'en'
-});
-$.i18n().load({
-  ru: 'static/ru.json',
-  en: 'static/ru.json',
-});
-
-$(document).ready(function () {
-  $.i18n().locale = 'ru';
+$(document).ready(function(){
+	$.getJSON('static/ru.json').done(function(data){
   $($('body > div > div > div > div > table > tbody')[0].children).each(function () {
-      let value = $(this)[0].children[0].textContent;
-      $(this)[0].children[0].textContent = $.i18n(value);
-      //console.log($(this)[0].children[0].textContent);
-  })
-
-  $('.dataframe').dataTable({
-      paging: true,
-      // scrollY: 600
+    let value = $(this)[0].children[0].innerHTML;
+	  $(this)[0].children[0].innerHTML = data[value.trim()] + ' ';
   });
+        $($('body > div > div > div > div > table > thead > tr')[0].children).each(function () {
+            let value = $(this)[0].innerHTML;
+            $(this)[0].innerHTML = data[value.trim()] + ' ';
+        });
 
-});
-
-console.log(jsonData);
-let json = $.getJSON(jsonData);
-console.log(json);
-var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
-
-
-// set the ranges
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-
-var y = d3.scale.linear().range([height, 0]);
-
-// define the axis
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
-
-
-// add the SVG element
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-
-// load the data
-d3.json(json, function(error, data) {
-
-    data.forEach(function(d) {
-        d.Letter = d.Letter;
-        d.Freq = +d.Freq;
-    });
-
-  // scale the range of the data
-  x.domain(data.map(function(d) { return d.Letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.Freq; })]);
-
-  // add axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 5)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Frequency");
-
-
-  // Add bar chart
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.Letter); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.Freq); })
-      .attr("height", function(d) { return height - y(d.Freq); });
-
-});
+  $('.dataframe').DataTable({
+      paging: true,
+  })
+})
+})
